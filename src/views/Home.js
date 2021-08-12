f3f1acb14ed1118c439a06c9b6d0b259fe05bab6
Chris <norish.chris@gmail.com>
2021-08-12 10:22:14 +0100

    Added useEffectto Home.js for API call to get widget list

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 const Home = () => {
   const history = useHistory();
+
+  const [widgetList, setwidgetList] = useState([]);
+
+  // API call
+  useEffect(() => {
+    (async () => {
+      let widgetResult;
+      try {
+        // widgetList = await getCompanyKeyAPI();
+        setwidgetList(widgetResult);
+      } catch (e) {
+        console.log('Get widget list API error = ', e);
+      }
+    })();
+  }, []);
+
   return (
-    <Container>
-      <Row>
-        <Col>
-          <h1>React Interview Project</h1>
-          <hr />
-          <Button>TEST</Button>
-        </Col>
-      </Row>
-    </Container>
+    <div>
+      <h1>All widgets</h1>
+      <hr />
+    </div>
   );
 };
 
